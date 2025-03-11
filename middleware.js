0c97d2b72586a323e90cd51f19db1126c17c4d2d
kushwahadheeraj <kushwahadheeraj245@gmail.com>
2025-03-11 17:59:02 +0530

    third

--- middleware.js
+++ middleware.js
@@ -1,6 +1,60 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+// import { clerkMiddleware, createClerkClient } from "@clerk/nextjs/server";
+// import { NextResponse } from "next/server";
 
-export default clerkMiddleware();
+// const isProtectedRoute = createClerkClient([
+//   "/dashboard(.*)",
+//   "/account(.*)",
+//   "/transaction(.*)",   
+// ]);
+
+// export default clerkMiddleware(
+//   async (auth, req) => {
+//   const { userId } = await auth();
+
+//   if (!userId && isProtectedRoute(req)) {
+//     const { redirectToSignIn } = await auth();
+//     return redirectToSignIn();
+//   }
+
+//   return NextResponse.next();
+// }
+// );
+
+// export const config = {
+//   matcher: [
+//     // Skip Next.js internals and all static files, unless found in search params
+//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+//     // Always run for API routes
+//     '/(api|trpc)(.*)',
+//   ],
+// };
+
+import { clerkMiddleware, createClerkClient } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+const protectedRoutes = [
+  "/dashboard",
+  "/account",
+  "/transaction",
+];
+
+const isProtectedRoute = (req) => {
+  const url = new URL(req.url);
+  return protectedRoutes.some(route => url.pathname.startsWith(route));
+};
+
+export default clerkMiddleware(
+  async (auth, req) => {
+    const { userId } = await auth();
+
+    if (!userId && isProtectedRoute(req)) {
+      const { redirectToSignIn } = await auth();
+      return redirectToSignIn();
+    }
+
+    return NextResponse.next();
+  }
+);
 
 export const config = {
   matcher: [
