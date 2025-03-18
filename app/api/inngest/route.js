b179a2c5cf919b3e21d1f49bee3fc270d8997721
kushwahadheeraj <kushwahadheeraj245@gmail.com>
2025-03-18 15:04:20 +0530

    Dashboard-forth

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -17,3 +17,15 @@ export const { GET, POST, PUT } = serve({
     checkBudgetAlerts,
   ],
 });
+
+// import { serve } from "inngest/next";
+// import {inngest} from "@/lib/inngest/client";
+// import { helloWorld } from "@/lib/inngest/function";
+
+// export const { GET, POST, PUT } = serve({
+//   client: inngest,
+//   functions: [
+//     helloWorld,
+//   ],
+// });
+
